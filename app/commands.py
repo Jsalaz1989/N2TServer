78a74f6b39996e6d548e5124308a56b6cf9f4b15
Jaime Salazar <jsalaz1989@gmail.com>
2019-10-18 13:33:55 +0200

    Changed app.config to current_app.config

--- app/commands.py
+++ app/commands.py
@@ -4,8 +4,10 @@ from flask.cli import with_appcontext
 from app.models import db
 # from .models import User, Question
 
+from flask import current_app
+
 @click.command(name='create_tables')
 @with_appcontext
 def create_tables():
-    print('app.config["SQLALCHEMY_DATABASE_URI"] = ', app.config['SQLALCHEMY_DATABASE_URI'])
+    print('current_app.config["SQLALCHEMY_DATABASE_URI"] = ', current_app.config['SQLALCHEMY_DATABASE_URI'])
     db.create_all()
