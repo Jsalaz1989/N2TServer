8c91c3012e281ea75e59498e368934184dc2320a
Jaime Salazar <jsalaz1989@gmail.com>
2019-10-21 11:54:41 +0200

    Redirecting to favicon in static folder

--- app/blueprints/home/routes.py
+++ app/blueprints/home/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, send_from_directory
+import os
 
 from . import homeBlueprint
 
@@ -7,3 +8,10 @@ from . import homeBlueprint
 def index():
 
     return render_template('index.html')
+
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
