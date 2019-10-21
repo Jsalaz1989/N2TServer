e2761e97e55cc4c2b4284a9987c2dc9b4fc0417c
Jaime Salazar <jsalaz1989@gmail.com>
2019-10-21 12:08:41 +0200

    There was no app in app.root_path, using current_app now

--- app/blueprints/home/routes.py
+++ app/blueprints/home/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, send_from_directory
+from flask import render_template, url_for, send_from_directory, current_app
 import os
 
 from . import homeBlueprint
@@ -13,4 +13,4 @@ def index():
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(current_app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
