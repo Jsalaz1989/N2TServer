f738d56d2d1636dab4f1a2626d0a13d4bf4a7021
Jaime Salazar <jsalaz1989@gmail.com>
2019-10-21 12:21:08 +0200

    Trying with favicon at route

--- app/blueprints/home/routes.py
+++ app/blueprints/home/routes.py
@@ -11,6 +11,6 @@ def index():
 
 
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(current_app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
+# @app.route('/favicon.ico')
+# def favicon():
+#     return send_from_directory(os.path.join(current_app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
